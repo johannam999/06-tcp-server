1abce27889d16df5da79101ba82e3acec5955bd9
Joanna Coll <johannam999@gmail.com>
2018-04-30 09:03:24 -0700

    update

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const winston = require('winston');
+
 const logger = module.exports = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename:' log.log', level: 'verbose'}),
-    new winston.transports.Console({ format: winston.format.simple(), level: 'info'}),   
+    new winston.transports.File({ filename: 'log.log', level: 'verbose' }),
+    new winston.transports.Console({ format: winston.format.simple(), level: 'info' }),   
   ],
 });
 
--- src/lib/server.js
+++ src/lib/server.js
@@ -3,11 +3,23 @@
 const net = require('net');
 const logger = require('./logger');
 const faker = require('faker');
+const uuid = require('uuid');
 
 const app = net.createServer();
-let clients = [];
 
-const parseCommand = (message, socket) => {
+const server = module.exports = {};
+
+let clientList = [];
+
+class Client {
+  constructor(socket) {
+    this.id = uuid();
+    this.nickname = faker.name.firstName();
+    this.socket = socket;
+  }
+}
+
+const parseCommand = (message, client) => {
   if (!message.startsWith('@')) {
     return false;
   }
@@ -17,52 +29,66 @@ const parseCommand = (message, socket) => {
 
   switch (command) {
     case '@list': {
-    const clientNanmes = clients.map(client => client.name).join('\n');
-    socket.write(`${clientNames}\n`);
-    break;
+      const clientNames = clientList.map(user => user.nickname).join('\n');
+      client.socket.write(`${clientNames}\n`);
+      break;
+    }
+    case '@quit': {
+      client.socket.end();
+      break;
+    }
+    case '@nickname': {
+      client.nickname = parsedMessage[1];
+      client.socket.write();
+      break;
+    }
+    case '@dm': {
+      const name = parsedMessage[1];
+      const receiver = clientList.filter(user => user.nickname === name)[0];
+      const msg = parsedMessage.slice(2).join(' ');
+      receiver.socket.write(`${client.nickname}: ${msg}`);
+      break;
     }
     default:
-    socket.write('wrong command');
-    break;
+      client.socket.write('wrong command');
+      break;
   }
   return true;
-}
+};
 
-const removeClient = socket => () => {
-  clients = clients.filter(client => client !==socket);
-  logger.log(logger.INFO, `remove ${socket.name}`);
+const removeClient = client => () => {
+  clientList = clientList.filter(user => user.id !== client.id);
+  logger.log(logger.INFO, `remove ${client.nickname}`);
 };
 
 app.on('connection', (socket) => {
   logger.log(logger.INFO, 'new socket');
-  clients.push(socket);
-  socket.write('this is your chat\n');
-  socket.name = faker.internet.userName();
-  socket.write(`your name is ${socket.name}\n`);
-  soocket.on('data', (data) => {
+  socket.write('Hi, welcome to this chat\n');
+  const client = new Client(socket);
+  clientList.push(client);
+ 
+  socket.write(`your name is ${client.id} and ${client.nickname}\n`);
+  socket.on('data', (data) => {
     const message = data.toString().trim();
-    logger.log(logger.INFO, `processing  msg: ${message}`);
-    if (parseCommand (message, cosket)) {
+    logger.log(logger.INFO, `Processing a message: ${message}`);
+    if (parseCommand(message, client)) {
       return;
     }
-    clients.forEach(client) => {
-      if (client !== socket) {
-        client.write(`${socket.name}: ${message}\n`);
-      }
+    clientList.forEach((user) => {
+      user.socket.write(`${client.nickname}: ${message}\n`);
     });
   });
-  socket.on('close', removeClient(socket));
+  socket.on('close', removeClient(client));
   socket.on('error', () => {
     logger.log(logger.ERROR, socket.name);
-    removeClient(socket)();
+    removeClient(client)();
   });
 });
 
-const server = module.exports = {};
 server.start = () => {
   if (!process.env.PORT) {
     logger.log(logger.ERROR, 'missing  Port');
-    throw new Error ('missing Port');
+    throw new Error('missing Port');
   }
   logger.log(logger.INFO, `Server is up on Port ${process.env.PORT}`);
   return app.listen({ port: process.env.PORT }, () => {});
